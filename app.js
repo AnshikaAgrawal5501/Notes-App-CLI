5b754384eafb4e73c6cd6e973383a7c3fa3a9c49
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-08-24 20:32:21 +0530

    notes app added

---
+++ app.js
@@ -0,0 +1,95 @@
+var validator = require('validator');
+const chalk = require('chalk');
+const notes = require('./notes');
+const yargs = require('yargs');
+
+// console.log(chalk.bold.cyan('Success'));
+
+// notes();
+
+yargs.command({
+    command: 'add',
+    description: 'Add a new note',
+    builder: {
+        title: {
+            description: 'Title of the note',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            description: 'Body of the note',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function(argv) {
+        console.log('adding a new note !');
+        // console.log(`title: ${argv.title}`);
+        // console.log(`body: ${argv.body}`);
+
+        const isAdded = notes.addNote(argv.title, argv.body);
+
+        if (isAdded) {
+            console.log(chalk.bold.green('Note successfully added.'));
+        } else {
+            console.log(chalk.bold.red('Note already present'));
+        }
+    }
+});
+
+yargs.command({
+    command: 'delete',
+    description: 'Delete a note',
+    builder: {
+        title: {
+            description: 'Title of note to be removed',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function(argv) {
+        const isRemoved = notes.removeNote(argv.title);
+
+        if (isRemoved) {
+            console.log(chalk.bold.green.inverse('deleteing a note !'));
+        } else {
+            console.log(chalk.bold.red.inverse('Note not found !'));
+        }
+    }
+});
+
+yargs.command({
+    command: 'read',
+    description: 'Reading a note',
+    builder: {
+        title: {
+            description: 'Title of note to be read',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function(argv) {
+        // console.log('reading a note !');
+        const isRead = notes.readNote(argv.title);
+
+        if (isRead) {
+            console.log(chalk.bold.green('Note successfully read.'));
+        } else {
+            console.log(chalk.bold.red('Note not present'));
+        }
+    }
+});
+
+yargs.command({
+    command: 'list',
+    description: 'List notes',
+    handler: function() {
+        console.log('listing all notes !');
+        notes.listNotes();
+    }
+});
+
+// console.log(process.argv);
+// console.log(yargs.argv);
+
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+
+function notes() {
+    console.log("notes ...");
+}
+
+function addNote(title, body) {
+
+    const notes = loadNotes();
+
+    const findNote = notes.filter(function(note) {
+        return note.title === title;
+    });
+
+    if (findNote.length > 0) {
+        return false;
+    }
+
+    const note = { title, body };
+    notes.push(note);
+
+    saveNotes(notes);
+    return true;
+}
+
+function removeNote(title) {
+    const notes = loadNotes();
+
+    const findOtherNote = notes.filter(function(note) {
+        return note.title !== title;
+    });
+
+    if (notes.length > findOtherNote.length) {
+        saveNotes(findOtherNote);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function readNote(title) {
+    const notes = loadNotes();
+
+    const findNote = notes.filter(function(note) {
+        return note.title === title;
+    });
+
+    if (findNote.length > 0) {
+        console.log(findNote[0].body);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function listNotes() {
+    const notes = loadNotes();
+
+    notes.forEach(function(note) {
+        console.log(`title: ${note.title}, body: ${note.body}`);
+    });
+}
+
+function saveNotes(notes) {
+    const notesString = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', notesString);
+}
+
+function loadNotes() {
+
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const jsonData = dataBuffer.toString();
+        return JSON.parse(jsonData);
+
+    } catch (e) {
+        return [];
+    }
+}
+
+module.exports = { notes, addNote, removeNote, readNote, listNotes };
